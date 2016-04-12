1e13abd4e09c5f627b22640f0ea81e68eb40ff3b
Rich Daley <rich@fishpercolator.co.uk>
2016-04-12 16:21:39 +0100

    Some silly stylistic stuff just to show what's possible

--- app/helpers/dataset_helper.rb
+++ app/helpers/dataset_helper.rb
@@ -1,2 +1,16 @@
 module DatasetHelper
+  def count_color(count)
+    case count
+    when 0..200
+      '#75A5D6'
+    when 201..500
+      '#628AB3'
+    when 501..1000
+      '#3F5973'
+    when 1000..2000
+      '#4D6C8C'
+    else
+      '#34495E'
+    end
+  end
 end
